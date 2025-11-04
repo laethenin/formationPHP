1bbcb747687e199ca52dbdd0d29b2e7fefcd60c3
laethenin <laetitiahenin@gmail.com>
2025-11-04 17:19:58 +0100

    ajout exo 3

--- Country.php
+++ Country.php
@@ -1,15 +1,35 @@
 <?php
 class Country {
-    public $name;
-    public $capital;
-    public $population;
-    public $continent;
+    private $name;
+    private $capital;
+    private $population;
+    private $continent;
 
     public function __construct($name, $capital, $population, $continent) {
         $this->name = $name;
         $this->capital = $capital;
         $this->population = $population;
         $this->continent = $continent;
+    }
+
+    public function getName() {
+        return $this->name . "<br>";
+    }
+    public function getCapital() {
+        return $this->capital . "<br>";
+    }
+    public function getPopulation() {
+        return $this->population . "<br>";
+    }
+    public function getContinent() {
+        return $this->continent . "<br>";
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function setPopulation($population) {
+        $this->population = $population;
     }
     public function getInfo(){
         echo "Le nom du pays est : " . $this->name . "<br>" . "Sa capitale est : " . $this->capital . "<br>" . "Sa population est de : " . $this->population . " millions d'habitants" . "<br>" . "Il est situé sur le continent : " . $this->continent . "<br><br>";
--- test_country.php
+++ test_country.php
@@ -2,16 +2,36 @@
 require_once 'Country.php';
 
 $country1 = new Country ("France", "Paris", "68", "Europe");
+echo $country1->getPopulation();
+$country1->getInfo();
+$country1->setName("Francia");
 $country1->getInfo();
 
+
 $country2 = new Country ("Japon", "Tokyo", "120", "Asie");
+echo $country2->getName();
+$country2->getInfo();
+$country2->setName("Nihon");
 $country2->getInfo();
 
+
 $country3 = new Country ("Maroc", "Rabat", "38", "Afrique");
+echo $country3->getCapital();
+$country3->getInfo();
+$country3->setPopulation("40");
 $country3->getInfo();
 
+
 $country4 = new Country ("Australie", "Canberra", "27", "Océanie");
+echo $country4->getContinent();
+$country4->getInfo();
+$country4->setPopulation("25");
 $country4->getInfo();
 
+
 $country5 = new Country ("Chili", "Santiago", "19", "Amérique");
+echo $country5->getName();
 $country5->getInfo();
+$country5->setName("Chile");
+$country5->getInfo();
+
